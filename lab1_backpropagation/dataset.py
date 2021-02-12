88e7977c081aa55505e56fee5686c309a7b5f41d
Fredrik <fredrik9779@gmail.com>
2021-02-12 19:17:57 +0100

    Added loading of dataset

--- lab1_backpropagation/dataset.py
+++ lab1_backpropagation/dataset.py
@@ -1,34 +1,71 @@
-import torchvision.transforms as transforms
-import os
+import h5py
+from pathlib import Path
+import torch
+from torch.utils.data import Dataset,DataLoader
+from easydict import EasyDict as edict
+
+
+class CustomDataLoader:
+    "Custom loader where we can init dataset for training, validation and testing"
+    def __init__(self, **hparams):
+        self.hparams = edict(hparams)
+        
+        
+    def train_dataloader(self):
+        ds = HDF5Dataset(self.hparams.dataset, step='training')
+        
+        dataloader = DataLoader(ds,
+                                batch_size=self.hparams.BATCH_SIZE,
+                                shuffle=True,
+                                num_workers=self.hparams.NUM_WORKERS)
+        return dataloader
+    
+    def validation_dataloader(self):
+        ds = HDF5Dataset(self.hparams.dataset, step='validation')
+        
+        dataloader = DataLoader(ds,
+                                batch_sizeself.hparams.BATCH_SIZE,
+                                shuffle=False,
+                                num_workers=self.hparams.NUM_WORKERS)
+        return dataloader
+    
+    def test_dataloader(self):
+        ds = HDF5Dataset(self.hparams.dataset, step='testing')
+        
+        dataloader = DataLoader(ds,
+                                batch_size=self.hparams.BATCH_SIZE,
+                                shuffle=False,
+                                num_workers=self.hparams.NUM_WORKERS)
+        return dataloader
 
 class HDF5Dataset(Dataset):
+    def __init__(self, file_path, step='training',transform=None):
+        super().__init__()
+        self.transform = transform
+    
+        p = Path(file_path)
+        assert(p.is_file())
+        df = h5py.File(p, 'r')
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    def __init__(self, h5_path=None):
-        assert os.path.isdir(h5_path), "Path to files does not exist!"
+        assert step in list(df.keys()), "Could not find step in keys!"
+        self.data = df[step]['data']
+        self.labels = df[step]['label']
         
-        self.train = test_catvnoncat.h5(h5_path, 'r')
-        self.length = 
-
-    def __getitem__(self, index): #to enable indexing
-        record = self.train[str(index)]
-        image = record['X'].value
-
-        # transform to PIL image
-        image = Image.fromarray(pixels.astype('uint8'), 'RGB') # assume your data is  uint8 rgb
-        label = record['y'].value
-
-        # transformation here
-        # torchvision PIL transformations accepts one image as input
-        image = self.transform(image)
-        return (
-                image,
-                label,
-        )
+    def __getitem__(self, index):
+        # get data
+        x = self.data[index].T # Transpose to pytorch input
+        if self.transform:
+            x = self.transform(x)
+        else:
+            x = torch.from_numpy(x)
+        x = x.float()
+        # get label
+        y = self.labels[index]
+        y = torch.from_numpy(y)
+        return (x, y)
 
     def __len__(self):
-        return len(h5py.File(h5_path, 'r')) 
+        return self.data.shape[0]
+    
+    def close(self):
+        self.df.close()
