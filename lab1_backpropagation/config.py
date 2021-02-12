a8a2c935d5b402a13c8eca48676d7bb8b15bebdd
Fredrik <fredrik9779@gmail.com>
2021-02-12 19:17:31 +0100

    Simplified config for lab1

--- lab1_backpropagation/config.py
+++ lab1_backpropagation/config.py
@@ -5,41 +5,17 @@ def flying_objects_config():
 
     cfg = edict()
 
-    cfg.training_data_dir = "../data/FlyingObjectDataset_10K/training"
-    cfg.validation_data_dir = "../data/FlyingObjectDataset_10K/validation"
-    cfg.testing_data_dir = "../data/FlyingObjectDataset_10K/testing"
-    cfg.fineGrained = False
+    cfg.dataset = "../data/flyingData_lab1.hdf5"
 
-    if cfg.fineGrained:
-      # classes
-      cfg.CLASSES = [
-          'square_red',
-          'square_green',
-          'square_blue',
-          'square_yellow',
-          'triangle_red',
-          'triangle_green',
-          'triangle_blue',
-          'triangle_yellow',
-          'circular_red',
-          'circular_green',
-          'circular_blue',
-          'circular_yellow']
-    else:
-      # classes
-      cfg.CLASSES = [
-          'square',
-          'triangle',
-          'circular']
-
-    cfg.NUM_CLASS = len(cfg.CLASSES)    # number of classes
-
+    #cfg.NUM_CLASS = len(cfg.CLASSES)    # number of classes
     cfg.GPU = 0                 # GPU ID or set as -1 in case of using CPU
+    cfg.NUM_CLASS=12
     cfg.DROPOUT_PROB = 0.5      # Probability to keep a node in dropout
     cfg.IMAGE_WIDTH = 128       # image width
     cfg.IMAGE_HEIGHT = 128      # image height
     cfg.IMAGE_CHANNEL = 3       # image channel
     cfg.NUM_EPOCHS = 10        # epoch number
+    cfg.NUM_WORKERS = 4
     cfg.BATCH_SIZE = 32         # batch size
     cfg.LEARNING_RATE = 0.001  # learning rate
     cfg.LR_DECAY_FACTOR = 0.1   # multiply the learning rate by this factor
