8f7dc1ef17ddb7f131aa3e7364b1971fafae0480
Fredrik <fredrik9779@gmail.com>
2021-06-29 20:38:04 +0200

    Updates to lab1

--- lab1_Backpropagation/config.py
+++ lab1_Backpropagation/config.py
@@ -6,8 +6,8 @@ def flying_objects_config():
     cfg = edict()
     cfg.NUM_CLASS = 10    # number of classes
     cfg.GPU = -1                 # GPU ID or set as -1 in case of using any GPU
-    cfg.IMAGE_WIDTH = 64       # image width
-    cfg.IMAGE_HEIGHT = 64      # image height
+    cfg.IMAGE_WIDTH = 28       # image width
+    cfg.IMAGE_HEIGHT = 28      # image height
     cfg.IMAGE_CHANNEL = 3       # image channel
     cfg.NUM_WORKERS = 4
     cfg.BATCH_SIZE = 32         # batch size
